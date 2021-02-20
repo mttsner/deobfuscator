5f3bf7857508d61cae1d12c0677656db5f9a5c26
notnoobmaster <42277794+notnoobmaster@users.noreply.github.com>
2021-02-20 14:43:41 +0000

    stuff

--- obfuscators/ironbrew/getvmdata.go
+++ obfuscators/ironbrew/getvmdata.go
@@ -1,9 +1,10 @@
 package ironbrew
 
 import (
+	// assigned to _ because go:embed doesn't work without requiring embed
+	_ "embed"
 	"errors"
 	"strconv"
-	_ "embed"
 	"strings"
 
 	"github.com/notnoobmaster/beautifier"
